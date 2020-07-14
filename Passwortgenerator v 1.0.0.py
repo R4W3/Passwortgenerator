81dee610811e63eaada930e7bd46f1a40ca2596b
R4W3 <rene.ehlers199902@gmail.com>
2020-07-14 19:37:05 +0200

    Add files via upload

---
+++ Passwortgenerator v 1.0.0.py
@@ -0,0 +1,11 @@
+print("Passwortgenerator v 1.0.0")
+print("Ein Service der RE IT Dienstleistungen")
+print("Ein Service der RE IT Dienstleistungen")
+import random
+x = ["a", "b", "d", "c", "e", "f", "g", "h", "j", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "x", "z", "A", "B", "D", "C", "E", "F", "G", "H", "J", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Y", "X", "Z","1", "2", "3", "4", "5", "6", "7", "8", "9", "#", "§", "$", "!", "%", "&", "/", "(", ")", "=", "?"]
+random.shuffle(x)
+random.shuffle(x)
+length = input("Passwortlänge (ganzzahlig): ")
+y = random.sample(x, int(length))
+str1 = ''.join(y)
+print("Dein Passwort lautet:" , str1)
