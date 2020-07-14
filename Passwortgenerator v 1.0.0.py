0ddad1e03bc5eb7788adb38bf9b1d99b21c60f40
R4W3 <rene.ehlers199902@gmail.com>
2020-07-14 19:38:40 +0200

    Update Passwortgenerator v 1.0.0.py

--- Passwortgenerator v 1.0.0.py
+++ Passwortgenerator v 1.0.0.py
@@ -1,6 +1,6 @@
 print("Passwortgenerator v 1.0.0")
 print("Ein Service der RE IT Dienstleistungen")
-print("Ein Service der RE IT Dienstleistungen")
+print("https://github.com/R4W3/Passwortgenerator")
 import random
 x = ["a", "b", "d", "c", "e", "f", "g", "h", "j", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "x", "z", "A", "B", "D", "C", "E", "F", "G", "H", "J", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Y", "X", "Z","1", "2", "3", "4", "5", "6", "7", "8", "9", "#", "§", "$", "!", "%", "&", "/", "(", ")", "=", "?"]
 random.shuffle(x)
